2a81e49ec252c331801e968143a6f3afd36d6b67
UjjawalVerma88 <ujjawal222161@gmail.com>
2026-09-20 19:01:15 +0530

    Time: 14 ms (33.48%) | Memory: 47.5 MB (76.35%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        int duplicate = -1;
+        int missing = -1;
+
+        // Find duplicate
+        for (int num : nums) {
+            if (set.contains(num)) {
+                duplicate = num;
+            }
+            set.add(num);
+        }
+
+        // Find missing
+        for (int i = 1; i <= nums.length; i++) {
+            if (!set.contains(i)) {
+                missing = i;
+                break;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
