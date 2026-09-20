4945ad09ca181b9b834a1be77ede10d999bda56e
UjjawalVerma88 <ujjawal222161@gmail.com>
2026-09-20 17:59:34 +0530

    Time: 1 ms (100.00%) | Memory: 85.6 MB (26.06%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,33 +1,46 @@
+// class Solution {
+
+    // public static void swap(int[] nums, int i, int idx) {
+    //     int temp = nums[i];
+    //     nums[i] = nums[idx];
+    //     nums[idx] = temp;
+    // }
+
+    // public int findDuplicate(int[] nums) {
+
+    //     int i = 0;
+
+    //     while (i < nums.length) {
+
+    //         if (nums[i] != i + 1) {
+
+    //             int idx = nums[i] - 1;
+
+    //             // Duplicate found
+    //             if (nums[i] == nums[idx]) {
+    //                 return nums[i];
+    //             }
+
+    //             swap(nums, i, idx);
+
+    //         } else {
+    //             i++;
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+// }
+
 class Solution {
-
-    public static void swap(int[] nums, int i, int idx) {
-        int temp = nums[i];
-        nums[i] = nums[idx];
-        nums[idx] = temp;
-    }
-
     public int findDuplicate(int[] nums) {
-
-        int i = 0;
-
-        while (i < nums.length) {
-
-            if (nums[i] != i + 1) {
-
-                int idx = nums[i] - 1;
-
-                // Duplicate found
-                if (nums[i] == nums[idx]) {
-                    return nums[i];
-                }
-
-                swap(nums, i, idx);
-
-            } else {
-                i++;
+        boolean[] set=new boolean[nums.length+1];
+        for(int val:nums){
+            if(set[val]){
+                return val;
             }
+            set[val]=true;
         }
-
         return -1;
     }
 }
